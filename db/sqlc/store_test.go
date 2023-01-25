f3f0e42b3b1dbde896f31df642c97d661bcbbe1a
LuigiAzevedo <luigiazevedo97@gmail.com>
2023-01-25 17:05:51 -0300

    Add Money Transfer TX and unit test

---
+++ db/sqlc/store_test.go
@@ -0,0 +1,124 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"math"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTransferTx(t *testing.T) {
+	store := NewStore(testDb)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	// run n concurrent transfer transactions
+	n := 5
+	amount := "10.00"
+	floatAmount, _ := strconv.ParseFloat(amount, 64)
+	floatBalance1, _ := strconv.ParseFloat(account1.Balance, 64)
+	floatBalance2, _ := strconv.ParseFloat(account2.Balance, 64)
+
+	errs := make(chan error)
+	results := make(chan TransferTxResult)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			result, err := store.TransferTx(context.Background(), CreateTransferParams{
+				FromAccountID: account1.ID,
+				ToAccountID:   account2.ID,
+				Amount:        amount,
+			})
+
+			errs <- err
+			results <- result
+		}()
+	}
+
+	// check results
+	existed := make(map[float64]bool)
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+
+		result := <-results
+		require.NotEmpty(t, result)
+
+		// check transfer
+		transfer := result.Transfer
+		require.NotEmpty(t, transfer)
+		require.Equal(t, account1.ID, transfer.FromAccountID)
+		require.Equal(t, account2.ID, transfer.ToAccountID)
+		require.Equal(t, amount, transfer.Amount)
+		require.NotZero(t, transfer.ID)
+		require.NotZero(t, transfer.CreatedAt)
+
+		_, err = store.GetTransfer(context.Background(), transfer.ID)
+		require.NoError(t, err)
+
+		// check from entries
+		fromEntry := result.FromEntry
+		require.NotEmpty(t, fromEntry)
+		require.Equal(t, account1.ID, fromEntry.AccountID)
+		require.Equal(t, fmt.Sprint("-", amount), fromEntry.Amount)
+		require.NotZero(t, fromEntry.ID)
+		require.NotZero(t, fromEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), fromEntry.ID)
+		require.NoError(t, err)
+
+		// check to entries
+		toEntry := result.ToEntry
+		require.NotEmpty(t, toEntry)
+		require.Equal(t, account2.ID, toEntry.AccountID)
+		require.Equal(t, amount, toEntry.Amount)
+		require.NotZero(t, toEntry.ID)
+		require.NotZero(t, toEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), toEntry.ID)
+		require.NoError(t, err)
+
+		// check from accounts
+		fromAccount := result.FromAccount
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, account1.ID, fromAccount.ID)
+
+		// check to accounts
+		toAccount := result.ToAccount
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, account2.ID, toAccount.ID)
+
+		// check accounts' balance
+		fromAccountBalance, _ := strconv.ParseFloat(fromAccount.Balance, 64)
+		toAccountBalance, _ := strconv.ParseFloat(toAccount.Balance, 64)
+
+		diff1 := floatBalance1 - fromAccountBalance
+		diff1 = math.Round(diff1*100) / 100
+
+		diff2 := toAccountBalance - floatBalance2
+		diff2 = math.Round(diff2*100) / 100
+
+		require.Equal(t, diff1, diff2)
+		require.True(t, diff1 > 0)
+		require.True(t, math.Mod(diff1, floatAmount) == 0) // 1 * amount, 2 * amount, 3 * amount, ...., n * amount
+
+		k := diff1 / floatAmount
+
+		require.True(t, k >= 1 && k <= float64(n))
+		require.NotContains(t, existed, k)
+		existed[k] = true
+	}
+
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	require.Equal(t, fmt.Sprintf("%.2f", floatBalance1-float64(n)*floatAmount), updatedAccount1.Balance)
+	require.Equal(t, fmt.Sprintf("%.2f", floatBalance2+float64(n)*floatAmount), updatedAccount2.Balance)
+}
